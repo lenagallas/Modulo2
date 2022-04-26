7cf8a72bbeaeb003a3395b151f459d1a6f8f1bf5
lenagallas <helena.gallas@sou.inteli.edu.br>
2022-04-26 14:19:27 -0300

    tentando organizar tudo

--- Outro/SEMANA_01/02_TUTORIAL/app_11.js
+++ Outro/SEMANA_01/02_TUTORIAL/app_11.js
@@ -4,10 +4,10 @@ const port = 3011;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
+  res.end('<!DOCTYPE html> <head> <meta charset="UTF-8"> <title>Etapa 0 - INSTALACAO</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
+                       <h1> Etapa 1 - INSTALAÇÃO - Servidor Node.js </h1> \
                        <H2> Meu servidor NODE.js funciona!</H2> </div> \
               </body> \
            </html>');
